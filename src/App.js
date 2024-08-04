43062d9c97cd1dc15b34bcec04a3f73179660d1b
adarsh1114 <92520001+adarsh1114@users.noreply.github.com>
2024-08-04 21:05:31 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import EmployeeList from './EmployeeList';
-// import EmployeeDetails from './EmployeeDetails';
-// import AddEmployee from './AddEmployee';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<EmployeeList/>} />
-//         <Route path="/employees/add" element={<AddEmployee/>} />
-//         <Route path="/employees/:id" element={<EmployeeDetails/>} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EmployeeList from './EmployeeList';
